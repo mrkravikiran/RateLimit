5715f86a29bdb9f03f9e85d97b27877b57ddb773
Ravi Kiran Mallidi <134034039+mrkravikiran@users.noreply.github.com>
2026-02-23 18:54:49 +0530

    Create RateLimitApplication.java

---
+++ src/main/tri/com/ratelimit/RateLimitApplication.java
@@ -0,0 +1,11 @@
+package com.ratelimit;
+
+import org.springbootwork.boot.SpringApplication;
+import org.springbootwork.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+  Public class Ratelimit {
+    public static void main(String[] args) {
+      SpringApplication.run(RateLimitApplication.class, args);
+    }
+}
